12baf2a25216b01aa417eb9c127d6af3758d601e
Valexr <valexr@gmail.com>
2023-02-09 18:50:13 +0300

    add gh

--- src/components/Score.svelte
+++ src/components/Score.svelte
@@ -1,11 +1,22 @@
 <script lang="ts" context="module">
     import { score, sorted } from "$lib/stores";
     import { shuffle } from "$lib/actions.js";
+    import gh from "../svg/gh.svg";
 </script>
 
 <header>
     <h3 class="counter">Steps: {$score}</h3>
-    <h1>Tagy</h1>
+    <h1>
+        <a
+            href="https://github.com/Valexr/Tagy"
+            target="_blank"
+            rel="noreferrer"
+            id="gh"
+        >
+            {@html gh}
+        </a>
+        Tagy
+    </h1>
     <nav>
         <button
             on:click={() => shuffle($sorted)}
